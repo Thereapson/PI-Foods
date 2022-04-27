844ee7fecd91cd3c0664af7064d2bf71e7b44262
sebastian <thereapson@gmail.com>
2022-04-27 08:58:52 -0500

    api27/04

--- api/src/models/Recipe.js
+++ api/src/models/Recipe.js
@@ -4,9 +4,23 @@ const { DataTypes } = require('sequelize');
 module.exports = (sequelize) => {
   // defino el modelo
   sequelize.define('recipe', {
-    name: {
+    tittle: {
       type: DataTypes.STRING,
       allowNull: false,
     },
+    id: {
+      type: DataTypes.UUID,
+      allowNull: false,
+      primarykey: true
+    },
+    summary: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    healtScore: { type: DataTypes.INTEGER },
+    puntuation: { type: DataTypes.INTEGER },
+    img: { type: DataTypes.TEXT },
+    instructions: { type: DataTypes.TEXT }
+
   });
 };
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,12 +1,16 @@
 const { Router } = require('express');
+const router = Router();
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
-
-
-const router = Router();
+const recipe = require('./recipe');
+const recipes = require('./recipes');
+const diets = require('./diets');
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+router.use("recipe", recipe);
+router.use("recipes", recipes);
+router.use("diets", diets);
 
 
 module.exports = router;
